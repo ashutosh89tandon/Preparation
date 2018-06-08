25a77614238a3953a490eb861fae6e0fcca12ce0
Ashutosh Tandon <ashtando@publicisgroupe.net>
2018-06-08 12:55:22 +0530

    Project created

---
+++ Preparation/src/com/prep/LeastCommonAncestor.java
@@ -0,0 +1,38 @@
+package com.prep;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LeastCommonAncestor
+{
+	public static void main(String[] args) {
+		
+		Node node=new Node(10);
+		node.setLeft(new Node(5));
+		node.setRight(new Node(15));
+		node.getLeft().setLeft(new Node(3));
+		node.getLeft().setRight(new Node(7));
+		node.getRight().setLeft(new Node(13));
+		node.getRight().setRight(new Node(17));
+		
+		Node result=LCA(node, Arrays.asList(node.getLeft().getRight(),node.getLeft().getLeft()));
+		System.out.println(result);
+	}
+	
+	private static Node LCA(Node node, List<Node> nodes)
+	{
+		if(node ==null)
+			return null;
+		
+		if(nodes.contains(node))
+			return node;
+		
+		Node left=LCA(node.getLeft(),nodes);
+		Node right = LCA(node.getRight(),nodes);
+		
+		if(left!=null && right!=null)
+			return node;
+		
+		return left==null?right:left;
+	}
+}
---
+++ Preparation/src/com/prep/Node.java
@@ -0,0 +1,70 @@
+package com.prep;
+
+public class Node
+{
+	private int data;
+	private Node left;
+	private Node right;
+	private Node next;
+	public Node(int data, Node left, Node right, Node next) {
+		super();
+		this.data = data;
+		this.left = left;
+		this.right = right;
+		this.next = next;
+	}
+	public Node(int data) {
+		super();
+		this.data = data;
+	}
+	public int getData() {
+		return data;
+	}
+	public void setData(int data) {
+		this.data = data;
+	}
+	public Node getLeft() {
+		return left;
+	}
+	public void setLeft(Node left) {
+		this.left = left;
+	}
+	public Node getRight() {
+		return right;
+	}
+	public void setRight(Node right) {
+		this.right = right;
+	}
+	public Node getNext() {
+		return next;
+	}
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	@Override
+	public String toString() {
+		return "Node [data=" + data + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + data;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Node other = (Node) obj;
+		if (data != other.data)
+			return false;
+		return true;
+	}
+	
+	
+}
