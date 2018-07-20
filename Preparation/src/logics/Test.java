bb00370de9b5943f465e4a249fc8335ac5e42b9d
Ashutosh Tandon <ashtando@publicisgroupe.net>
2018-07-20 15:14:25 +0530

    Add

--- Preparation/src/logics/Test.java
+++ Preparation/src/logics/Test.java
@@ -5,5 +5,11 @@ public class Test
 	public static void main(String args[])
 	{
 		int i=00007;
+		
+		double a=3.2;
+		double b=3.20;
+		double c=(Math.round(3.1999999999 *100.0))/100.0;
+		System.out.println(c);
+		System.out.println(a==b);
 	}
 }
