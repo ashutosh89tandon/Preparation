6c2edafa238e9576445206653057971585eec8d6
ashutosh89tandon <ashutosh89tandon@gmail.com>
2018-10-28 15:40:51 +0530

    Add

--- Preparation/src/sorting/QuickSortMyVersion.java
+++ Preparation/src/sorting/QuickSortMyVersion.java
@@ -3,7 +3,7 @@ package sorting;
 public class QuickSortMyVersion {
 	
 	public static void main(String[] args) {
-		int a[]= {6,3,9,1,6,7,3,4,9};
+		int a[]= {6,4,2,8,7,8,9,1,23,7,6};
 		quickSort(a,0,a.length-1);
 		
 		for(int i=0;i<a.length;i++)
@@ -15,7 +15,7 @@ public class QuickSortMyVersion {
 		if(start<end)
 		{
 			int partition=partition(a, start, end);
-			quickSort(a, start, partition-1);
+			quickSort(a, start, partition);
 			quickSort(a, partition+1, end);
 		}
 		
@@ -25,17 +25,37 @@ public class QuickSortMyVersion {
 		
 		int pivot=a[end];
 		int left=start;
+		int index=-1;
 		while(left<end)
 		{
 			if(a[left]>pivot)
 			{
-				int temp =a[left];
-				a[left]=pivot;
-				pivot=temp;
+				swap(a, left, end);
+				pivot=a[end];
+				index=left;
 			}
 			left++;
 		}
+		
+		
+		if(index==-1)
+			index=end-1;
+		else {
+			for(int i=index;i<end-1;i++)
+				if(a[i]>a[i+1])
+				{
+					index=i+1;
+					swap(a,i+1,i);
+				}
+		}
 		a[end]=pivot;
-		return end;
+		return index;
+	}
+	
+	private static void swap(int a[],int i, int j)
+	{
+		int temp=a[i];
+		a[i]=a[j];
+		a[j]=temp;
 	}
 }
