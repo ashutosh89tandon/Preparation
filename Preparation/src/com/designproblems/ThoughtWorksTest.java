8a6637e458437afd706713e74110f240ce759814
Ashutosh Tandon <ashtando@publicisgroupe.net>
2018-12-24 12:12:39 +0530

    Add

--- Preparation/src/com/designproblems/ThoughtWorksTest.java
+++ Preparation/src/com/designproblems/ThoughtWorksTest.java
@@ -2,7 +2,6 @@ package com.designproblems;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class ThoughtWorksTest 
 {
@@ -17,12 +16,10 @@ public class ThoughtWorksTest
 				int numberOfChildren=Integer.parseInt(br.readLine());
 				String[] array=boxes.split(" ");
 				int numberOfBoxes=Integer.parseInt(array[0]);
-				String boxArray[]=Arrays.copyOfRange(array, 1, array.length);
-				
 				if(numberOfBoxes<=0)
 					System.out.println("NO");
 				
-				if(isSubsetSum(boxArray, boxArray.length, numberOfChildren))
+				if(isSumAvailable(array, numberOfChildren))
 					System.out.println("YES");
 				else
 					System.out.println("NO");
@@ -36,50 +33,61 @@ public class ThoughtWorksTest
 		} 
 	}
 
-	/*static boolean isSubsetSum(String set[], int n, int sum) 
-	{ 
-		if (sum == 0) 
-			return true; 
-		if (n == 0 && sum != 0) 
-			return false; 
-		
-		if (Integer.parseInt(set[n-1]) > sum) 
-			return isSubsetSum(set, n-1, sum); 
-		return isSubsetSum(set, n-1, sum) ||  
-				isSubsetSum(set, n-1, sum-Integer.parseInt(set[n-1])); 
-	} */
 	
-	
-	static boolean isSubsetSum(String set[],  
-			int n, int sum) 
+	/*static boolean isSumAvailable(String boxArray[], int sum) 
 	{ 
-		 
-		boolean subset[][] =  
-				new boolean[sum+1][n+1]; 
-
-		
+		int n=boxArray.length;
+		boolean subset[][] =  new boolean[sum+1][n+1]; 
 		for (int i = 0; i <= n; i++) 
 			subset[0][i] = true; 
-
-		
 		for (int i = 1; i <= sum; i++) 
 			subset[i][0] = false; 
-
-		
 		for (int i = 1; i <= sum; i++) 
 		{ 
-			for (int j = 1; j <= n; j++) 
+			for (int j = 2; j <= n; j++) 
 			{ 
 				subset[i][j] = subset[i][j-1]; 
-				if (i >= Integer.parseInt(set[j-1])) 
+				if (i >= Integer.parseInt(boxArray[j-1])) 
 					subset[i][j] = subset[i][j] ||  
-					subset[i - Integer.parseInt(set[j-1])][j-1]; 
+					subset[i - Integer.parseInt(boxArray[j-1])][j-1]; 
 			} 
 		} 
-
 		return subset[sum][n]; 
-	} 
+	}*/
 	
-	
+	static boolean isSumAvailable(String boxArray[], int sum) 
+	{ 
+		int bit=1;
+		for(int i=1;i<boxArray.length;i++)
+		{
+			int arrValue=Integer.parseInt(boxArray[i]);
+			bit=bit|(bit<<arrValue);
+		}
+		
+		int bitArray[]=getBitArrayFromDecimal(bit);
+		
+		if(bitArray[sum]==1)
+			return true;
+		
+		return false;
+	}
 
+	private static int[] getBitArrayFromDecimal(int bit) {
+		int bitArray[]=new int[10000];
+		int bitOutput[]=new int[10000];
+		int length=0;
+		while(bit>0)
+		{
+			int rem=bit%2;
+			bitOutput[length++]=rem;
+			bit/=2;
+		}
+		
+		for(int i=length-1,j=0;i>=0;i--,j++)
+		{
+			bitArray[j]=bitOutput[i];
+		}
+		
+		return bitArray;
+	}
 }
