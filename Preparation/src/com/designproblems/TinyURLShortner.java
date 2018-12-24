e6c92f36d598754418c450d42c0c4a7471edbd65
Ashutosh Tandon <ashtando@publicisgroupe.net>
2018-12-24 13:11:12 +0530

    Add

--- Preparation/src/com/designproblems/TinyURLShortner.java
+++ Preparation/src/com/designproblems/TinyURLShortner.java
@@ -1,5 +1,34 @@
 package com.designproblems;
 
-public class TinyURLShortner {
-
+public class TinyURLShortner 
+{
+	private static String map="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	public static void main(String[] args) {
+		int n=786352847;
+		String shortUrl=getUrlFromId(n);
+		int id=getIDFromShortUrl(shortUrl);
+		System.out.println("shortUrl : "+shortUrl);
+		System.out.println("id : "+id);
+	}
+	private static int getIDFromShortUrl(String shortUrl) {
+		
+		int id=0;
+		for(int i=0;i<shortUrl.length();i++)
+		{
+			char ch=shortUrl.charAt(i);
+			id+=Math.pow(62, i)*(map.indexOf(ch));
+		}
+		return id;
+	}
+	private static String getUrlFromId(int n) {
+		String str="";
+		char[] charArray=map.toCharArray();
+		while(n>0)
+		{
+			str=str+charArray[n%62];
+			n=n/62;
+		}
+		
+		return str;
+	}
 }
